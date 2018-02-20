cff8ea5cc979d562df4c1810d61b363e61319923
Cédric Alfonsi <cedric@passbolt.com>
2018-02-20 17:23:56 +0530

    PASSBOLT-2610 Passwords workspace, filter by tags section, more menu should be represented with a dropdown

--- demo/includes/filters/LU_tags_filters.php
+++ demo/includes/filters/LU_tags_filters.php
@@ -8,9 +8,52 @@
                         <h3><a href="#" class="accordion-trigger">Filter by tags</a></h3>
                     </div>
                 </div>
-                <div class="right-cell more-ctrl">
-                    <a href="#" class="filter"><span>more</span></a>
+                <div class="right-cell more-ctrl mad_component_button_dropdown js_component mad_view_component_button_dropdown enabled">
+                    <a id="js_wsp_pwd_password_filter_tags_more" href="#" class="filter"><span>more</span></a>
+                    <ul id="js_wsp_pwd_password_filter_tags_more_list" class="dropdown-content mad_component_menu menu mad_view_component_tree ready">
+                        <li id="js_filter_tags_section_all_tags_options" class="ready">
+                            <div class="row">
+                                <div class="main-cell-wrapper">
+                                    <div class="main-cell">
+                                        <a><span>All tags</span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li id="js_filter_tags_section_my_tags_options" class="ready">
+                            <div class="row">
+                                <div class="main-cell-wrapper">
+                                    <div class="main-cell">
+                                        <a><span>My tags</span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li id="js_filter_tags_section_shared_tags_options" class="ready">
+                            <div class="row">
+                                <div class="main-cell-wrapper">
+                                    <div class="main-cell">
+                                        <a><span>Shared tags</span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
+                <script type="application/javascript">
+                    // SAMPLE INTERACTION - Not for production
+                    $(function() {
+                        $('#js_wsp_pwd_password_filter_tags_more').click(function(){
+                            $('#js_wsp_pwd_password_filter_tags_more_list').toggleClass('visible');
+                            return false;
+                        });
+                    });
+                    $('body').click(function(e) {
+                        if($(e.target).closest('#js_wsp_pwd_password_filter_tags_more').length === 0) {
+                            $('#js_wsp_pwd_password_filter_tags_more_list').removeClass('visible');
+                        }
+                    });
+                </script>
             </div>
         </li>
     </ul>
